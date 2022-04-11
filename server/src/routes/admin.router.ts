0d82be472bb019d5c99d08bdc59d08084095f0ac
Asahi Cantu <asahicantu@outlook.com>
2022-04-11 20:00:30 +0200

    Implemented endpoint for couchdb inserts

--- server/src/routes/admin.router.ts
+++ server/src/routes/admin.router.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction } from 'express';
+import CCParams from 'interfaces/ccPArams.interface';
 import Boom from 'boom';
 import AdminService from '../services/admin.service';
 import DbService from '../services/db.service';
@@ -22,45 +23,43 @@ class AdminRouter {
       res.json(info);
     });
 
-    this.router.post('/createdb', async(req,res)=> {
-        const dbName = req.query.name as string | undefined;
-        const response = await this.dbService.createDb(dbName);
-        res.json(response);
+    this.router.post('/register', async (req, res) => {
+      const config = req.body;
+      const response = await this.dbService.registerOrg(config);
+      res.json(response);
     });
 
 
     this.router.post('/mint', async (request, response, next) => {
+      let config: Record<string, any>;
       try {
-        const userId = request.query.userId as string;
-        const affiliation = request.query.affiliation as string;
-        const chaincode = request.query.chaincode as string;
-        const channel = request.query.channel as string;
-        const tokenId = request.query.tokenId as string;
-        const tokenUrl = request.query.tokenUrl as string;
-        const ccp: Record<string, any> = request.body;
-        if (!ccp) {
-          throw ("Missing connection profile as json body");
-        }
-        if (!ccp || !userId || !affiliation || !chaincode || !channel || !tokenId || !tokenUrl) {
-          throw (`Error, missing parameters required in URL userId=${userId}&affiliation=${affiliation}&chaincode:${chaincode}&channel:${channel}&tokenId${tokenId}&tokenUrl=${tokenUrl}`);
-        }
-
-        var caHostName = this.keyAt(ccp.certificateAuthorities, 0);
-        const mspId = this.elementAt(ccp.organizations, 0).mspid;
-        const caClient = this.adminService.buildCAClient(ccp, caHostName)
-        const url ='https://admin:adminpw@localhost:7984';
-        const filePath = 'wallet/org1';
-        const wallet = await this.adminService.buildWallet(undefined,url);
-        await this.adminService.enrollAdmin(caClient, wallet, mspId);
-        await this.adminService.registerAndEnrollUser(caClient, wallet, mspId, userId, affiliation)
-        console.log('executing contract...');
-        const result = await this.adminService.mint(ccp, wallet, userId, channel, chaincode, tokenId, tokenUrl);
-        response.send(result);
+        const orgConfig = request.query.config as string;
+        let cf = await this.dbService.GetConnectionConfig(orgConfig);
+        config = cf as Record<string, any>;
       }
       catch (error) {
-        next(error);
+        throw (error);
       }
+      const ccp = request.body as CCParams;
+      if (!ccp) {
+        throw ("Missing chaincode params");
+      }
+      var caHostName = this.keyAt(config.certificateAuthorities, 0);
+      const userId = ccp.userId;
+      const affiliation = ccp.affiliation;
+      const mspId = this.elementAt(config.organizations, 0).mspid;
+      const caClient = this.adminService.buildCAClient(config, caHostName)
+      const url = 'https://admin:adminpw@localhost:7984';
+      const wallet = await this.adminService.buildWallet(undefined, url);
+      await this.adminService.enrollAdmin(caClient, wallet, mspId);
+      await this.adminService.registerAndEnrollUser(caClient, wallet, mspId, userId, affiliation)
+      console.log('executing contract...');
+      const result = await this.adminService.mint(config, wallet, ccp.userId, ccp.channel, ccp.chaincode, ccp.tokenId, ccp.tokenUrl);
+      response.send(result);
     });
+
+
+
     this.router.post('/balance', async (request, response, next) => {
       try {
         const userId = request.query.userId as string;
@@ -79,8 +78,8 @@ class AdminRouter {
         var caHostName = this.keyAt(ccp.certificateAuthorities, 0);
         const mspId = this.elementAt(ccp.organizations, 0).mspid;
         const caClient = this.adminService.buildCAClient(ccp, caHostName)
-        const url ='https://admin:adminpw@localhost:7984';
-        const wallet = await this.adminService.buildWallet(undefined,url);
+        const url = 'https://admin:adminpw@localhost:7984';
+        const wallet = await this.adminService.buildWallet(undefined, url);
         await this.adminService.enrollAdmin(caClient, wallet, mspId);
         await this.adminService.registerAndEnrollUser(caClient, wallet, mspId, userId, affiliation)
         console.log('executing contract...');
--- server/src/services/db.service.ts
+++ server/src/services/db.service.ts
@@ -1,29 +1,60 @@
+import { Console } from 'console';
 import *  as Nano from 'nano';
 class DBService {
   nano: Nano.ServerScope;
+  db: Nano.DocumentScope<unknown>;
   constructor() {
     const port = process.env.SRV_DB_PORT as string;
-    const host =  process.env.SRV_DB_URL as string;
+    const host = process.env.SRV_DB_URL as string;
     const user = process.env.SRV_DB_USER_ID as string;
     const pw = process.env.SRV_DB_USER_PW as string;
     const url = `http://${user}:${pw}@${host}:${port}`;
+    const dbname = process.env.SRV_DB_DBNAME as string;
     this.nano = Nano.default(url);
+    //Build default database at runtime
+    this.nano.db.list()
+      .then((dbs: string[]) => {
+        if (!dbs.includes(dbname)) {
+          console.log(`Database ${dbname} not exists, creating....`);
+          this.createDb(dbname)
+        }
+      }).finally(() => {
+        this.db = this.nano.db.use(dbname);
+        console.log(`Database ${dbname} connected!`);
+      });
   }
 
-  async createDb(dbName: string | undefined) : Promise<Nano.DatabaseCreateResponse | string> {
-    if (!dbName) {
-      dbName = process.env.SRV_DB_DBNAME as string;
-    }
+  async createDb(dbName: string): Promise<Nano.DatabaseCreateResponse | string> {
     try {
+      console.log(`Datavase ${dbName} created`);
       return await this.nano.db.create(dbName);
     }
     catch {
-      return `Database ${dbName} already exists`;
+      const msg = `Database ${dbName} already exists`;
+      console.log(msg)
+      return msg;
     }
   }
 
-   dbInfo() {
-    return this.db.info();
+  async dbInfo(): Promise<Nano.InfoResponse> {
+    return await this.nano.info();
+  }
+
+  async registerOrg(connectionConfig: object): Promise<Nano.DocumentInsertResponse | unknown> {
+    try {
+      const response = await this.db.insert(connectionConfig);
+      return response;
+    }
+    catch (error) {
+      return error;
+    }
+  }
+
+  async GetConnectionConfig(orgConfig: string): Promise<unknown> {
+    {
+      const val: Nano.DocumentGetResponse = await this.db.get(orgConfig);
+      return val;
+    }
   }
 
 }
